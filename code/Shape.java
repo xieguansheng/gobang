f7c46e7e61fdbe814ec90324c195f209073cb22a
xieguansheng <1356112544@qq.com>
2019-09-17 12:56:05 +0800

    commit modification

---
+++ code/Shape.java
@@ -0,0 +1,10 @@
+package day0512Wuziqi;
+
+import java.awt.Color;
+
+public class Shape {
+	int x;
+	int y;
+	Color color;
+
+}
---
+++ code/WuziqiConfig.java
@@ -0,0 +1,16 @@
+package day0512Wuziqi;
+
+import java.util.HashMap;
+
+public interface WuziqiConfig {
+	
+	public int COUNT = 17;
+	public int size = 50;
+	public int margin_x = 40;
+	public int margin_y = 40;
+	public int width = 890;
+	public int height = 970;
+	
+
+	
+}
